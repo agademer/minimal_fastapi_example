8aa7cfebe505759f56c5facb054d832ad099cbc6
Antoine Gademer <antoine.gademer@epf.fr>
2025-10-13 20:32:03 +0200

    Improving comments in the code

--- app/main.py
+++ app/main.py
@@ -8,24 +8,28 @@ import os
 
 app = FastAPI(title="Minimal Fastapi Example", docs_url="/docs")
 
-app.mount("/public", StaticFiles(directory="public"), name="public")
+# The static dir is for file that should be accessed directly (ex: css, js, images)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# The template dir is for frontend file that can have parameters
 templates = Jinja2Templates(directory="app/templates")
 
+# Listing the images in the static/photos directory to give as parameter to the template
 photoList = []
-for filename in os.listdir('public/photos'):
+for filename in os.listdir('static/photos'):
     if filename.endswith(".jpg"):
-        photoList.append(os.path.join('public','photos', filename))
+        photoList.append(os.path.join('static','photos', filename))
     else:
         continue
 
-print(photoList)
-
+# Definition of the starting page (through templating)
 @app.get("/", include_in_schema=True)
 async def root(request: Request):
     return templates.TemplateResponse(request, "home.html", context={"photoList":photoList})
 
+# Running the actual app
 if __name__ == "__main__":
+    # Managing the binder specificity
     if "JUPYTERHUB_SERVICE_PREFIX" in os.environ.keys():
         BINDER_URL=f"{os.environ.get('JUPYTERHUB_SERVICE_PREFIX')}proxy/8000/"
         uvicorn.run(app, host="localhost", port=8000, root_path=BINDER_URL)
