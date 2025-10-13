72265edd40abbdd58d4568372c856cd3eddaa005
Antoine Gademer <antoine.gademer@epf.fr>
2025-10-13 18:56:28 +0200

    Correct root_path in Binder

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi.templating import Jinja2Templates
 import uvicorn
 import os
 
-app = FastAPI(title="Smoothie minimal test", docs_url="/docs")
+app = FastAPI(title="Minimal Fastapi Example", docs_url="/docs")
 
 app.mount("/public", StaticFiles(directory="public"), name="public")
 
@@ -21,9 +21,13 @@ for filename in os.listdir('public/photos'):
 
 print(photoList)
 
-@app.get("/", include_in_schema=False)
+@app.get("/", include_in_schema=True)
 async def root(request: Request):
     return templates.TemplateResponse(request, "home.html", context={"photoList":photoList})
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    if "JUPYTERHUB_SERVICE_PREFIX" in os.environ.keys():
+        BINDER_URL=f"{os.environ.get('JUPYTERHUB_SERVICE_PREFIX')}proxy/8000/"
+        uvicorn.run(app, host="localhost", port=8000, root_path=BINDER_URL)
+    else:
+        uvicorn.run(app, host="localhost", port=8000)
